393d16550ad13ef5b3b7d41b44ac46b225fc7268
IraV20 <110539098+IraV20@users.noreply.github.com>
2025-05-13 20:42:59 +0300

    Edit a contact in a component. Modal
    created a modal window for editing an existing contact. Change function.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,41 +2,80 @@ import css from './Contact.module.css';
 import { HiUser } from "react-icons/hi";
 import { FaPhoneAlt } from "react-icons/fa";
 import { useDispatch } from 'react-redux';
-import { deleteContact } from '../../redux/contacts/contactsOps';
-import { ContactModal } from '../deleteModal/deleteModal';
 import { useState } from 'react';
+import toast from 'react-hot-toast';
 
-function Contact({contact}) {
+import { deleteContact, updateContact } from '../../redux/contacts/contactsOps';
+import { ContactModal } from '../deleteModal/deleteModal';
+import { UpdateContactModal } from '../updateContactModal/updateContactModal';
+
+
+export function Contact({ contact }) {
 
     const dispatch = useDispatch();
-    const [isOpenModal, setIsOpenModal] = useState(false);
+    const [isOpenDeleteModal, setIsOpenDeleteModal] = useState(false);
+    const [isOpenUpdateModal, setIsOpenUpdateModal] = useState(false);
+
+    // For delete contact
     
+    function openDeleteModalClick() {
+        setIsOpenDeleteModal(true);
+    }
 
     const handleDeleteContact = () => {
         dispatch(deleteContact(contact.id))
-        setIsOpenModal(false);
-    }
-    
-    function openModalClick() {
-        setIsOpenModal(true);
+        setIsOpenDeleteModal(false);
+        toast.success('Contact was successfully deleted!')
     }
 
-    function closeModal() {
-        setIsOpenModal(false);
+    function closeDeleteModal() {
+        setIsOpenDeleteModal(false);
+    }
+
+    //For update
+    
+    function openUpdateModalClick() {
+        setIsOpenUpdateModal(true);
+    }
+
+    const handleUpdateContact = (values) => {
+        const updateData = {};
+
+        if (values.name !== contact.name) {
+            updateData.name = values.name;
+        }
+
+        if (values.number !== contact.number) {
+            updateData.number = values.number;
+        }
+
+        if (Object.keys(updateData).length === 0) {
+            toast('No changes were made');
+            return;
+        }
+        console.log(updateData);
+        
+        dispatch(updateContact({ contactId: contact.id, updateData }));
+        toast.success('The contact was successfully updated!')
+        setIsOpenUpdateModal(false);
+    };
+    
+
+    function closeUpdateModal() {
+        setIsOpenUpdateModal(false);
     }
     
-    
- return(
-    <div className={css.itemBox}>
-        <ul>
-            <li><p><HiUser className={css.icon}/>{contact.name}</p></li>
-            <li><p><FaPhoneAlt className={css.icon}/>{contact.number}</p></li>
-        </ul>
-        <button className={css.btn} onClick={openModalClick}>Delete</button>
-        <ContactModal isOpen={isOpenModal} onConfirm={handleDeleteContact} onReject={closeModal}/>
-    </div>
- )    
+    return (
+        <div className={css.itemBox}>
+            <ul>
+                <li><p><HiUser className={css.icon} />{contact.name}</p></li>
+                <li><p><FaPhoneAlt className={css.icon} />{contact.number}</p></li>
+            </ul>
+            <button className={css.btn} onClick={openDeleteModalClick}>Delete</button>
+            <button className={css.btn} onClick={openUpdateModalClick}>Update</button>
+            <ContactModal isOpen={isOpenDeleteModal} onConfirm={handleDeleteContact} onReject={closeDeleteModal} />
+            <UpdateContactModal isOpen={isOpenUpdateModal} handleSave={handleUpdateContact} contact={contact} onClose={closeUpdateModal} />
+        </div>
+    )
 
-}
-
-export default Contact;
+}   
---
+++ src/components/updateContactModal/updateContactModal.jsx
@@ -0,0 +1,70 @@
+import Modal from 'react-modal';
+import * as Yup from 'yup';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { useId } from 'react';
+
+import css from "./updateContactModal.module.css";
+
+const customStyles = {
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.6)'
+  },
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+Modal.setAppElement('#root');
+
+const ContactSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(3, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    number: Yup.string()
+      .matches(/^\d+$/, "Must be only digits")
+      .min(3, "Too Short!")
+      .max(8, "Too Long!")
+      .required("Required"),
+  });
+
+export const UpdateContactModal = ({ isOpen, handleSave, contact, onClose }) => {
+    
+    const fielId = useId();
+    
+    return (
+        <Modal
+        isOpen={isOpen}
+        contact={contact}
+        onRequestClose={onClose}
+        style={customStyles}
+        >
+        <Formik initialValues={{
+            name: contact.name, 
+            number: contact.number}} 
+            validationSchema={ContactSchema}
+            onSubmit={handleSave}>
+                <Form className={css.form}> 
+                <h2>Update Contact</h2>
+                <div className={css.formElem}>
+                <label htmlFor={`${fielId}-name`}>Name</label>
+                <Field className={css.formInput} type="text" name="name" id={`${fielId}-name`}></Field>
+                <ErrorMessage name='name' component='span' className={css.err}/>
+                </div>
+
+                <div  className={css.formElem}>
+                <label htmlFor={`${fielId}-number`}>Number</label>
+                <Field className={css.formInput} type="tel" name="number" id={`${fielId}-number`}></Field>
+                <ErrorMessage name='number' component='span' className={css.err}/>
+                </div>
+                
+                <button className={css.formBtn} type='submit'>Save</button>
+            </Form>
+        </Formik></Modal>
+    )
+}
